217d968a9003b6e34145ed3af028fb7181cfa02b
rozhevadimev <rozhevadim@gmail.com>
2023-02-02 11:51:14 +0700

    change return values to string

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-// задание 15
+﻿// задание 15
 Console.WriteLine("Please, Enter number");
 int number = int.Parse(Console.ReadLine ()!);
 
-bool f(int day) {
-    return day == 6 || day == 7;
+string f(int day) {
+    bool isWeekend = day == 6 || day == 7;
+    return isWeekend ? "Да" : "Нет";
 }
 Console.WriteLine(f(number));
