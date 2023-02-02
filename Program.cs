fb7eb56c6b8111c47a49bd28f692a903dcab63da
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-02 10:20:36 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Please, Enter number");
-int number = int.Parse(Console.ReadLine ()!);
 // задание 13
+Console.WriteLine("Please, Enter number");
+int number = int.Parse(Console.ReadLine ()!);
+
 dynamic f(int n) 
 {
     if (n < 100) {
