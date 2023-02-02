2829ebd6a98cba32633d2b961bb8b2b831393050
vinnikov <vinnikov@gim16.local>
2023-02-02 09:32:11 +0700

    task10

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please, Enter number");
+int number = int.Parse(Console.ReadLine ()!);
+// задание 10
+int f(int n) 
+{
+    return int.Parse(n.ToString().Substring(1, 1));
+}
+Console.WriteLine(f(number));
