5e8cb3942f4b8236b10c3d3524014231e1d261d4
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-02 10:21:05 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Please, Enter number");
-int number = int.Parse(Console.ReadLine ()!);
 // задание 15
+Console.WriteLine("Please, Enter number");
+int number = int.Parse(Console.ReadLine ()!);
+
 bool f(int day) {
     return day == 6 || day == 7;
 }
