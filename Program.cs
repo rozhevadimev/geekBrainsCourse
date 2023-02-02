d15f19fd7a48c01406d9df9e33636a4a07c516cf
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-02 10:20:03 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Please, Enter number");
+﻿// задание 10
+Console.WriteLine("Please, Enter number");
 int number = int.Parse(Console.ReadLine ()!);
-// задание 10
+
 int f(int n) 
 {
     return int.Parse(n.ToString().Substring(1, 1));
