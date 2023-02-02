d6ac5800f731e7590b3ff219584991bd2d4f2431
vinnikov <vinnikov@gim16.local>
2023-02-02 09:35:19 +0700

    task15

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿Console.WriteLine("Please, Enter number");
 int number = int.Parse(Console.ReadLine ()!);
-// задание 13
-dynamic f(int n) 
-{
-    if (n < 100) {
-        return "no third digit";
-    }
-    return int.Parse(n.ToString().Substring(2, 1));
+// задание 15
+bool f(int day) {
+    return day == 6 || day == 7;
 }
 Console.WriteLine(f(number));
