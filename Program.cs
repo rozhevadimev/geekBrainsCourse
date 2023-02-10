9fc3f2273f1d6f47596ce90a4674033d64fc3375
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-10 14:11:10 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,23 @@
-// Задание 23
+// Задание 4_3: Показать кубы чисел, заканчивающихся на четную цифру
 
 Console.WriteLine("Please, Enter number");
+
 int number = int.Parse(Console.ReadLine ()!);
 
+//Cubes of even numbers also end with an even digit
+
 int count = 0;
+
 Console.WriteLine($" {count}");
 
 while (count < number)
 
 {
+ 
+count += 2;
     
-count += 1;
 Console.WriteLine($"number {count}");
-//
+
 Console.WriteLine($"cube of number" + Math.Pow(count, 3)); 
 
  }
