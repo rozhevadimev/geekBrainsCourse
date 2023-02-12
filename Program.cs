be67c3ff9abfacf7aba13d6e1ca65659d42aff17
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-12 21:51:26 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-// Задание 23
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.WriteLine("Please, Enter number");
-int number = int.Parse(Console.ReadLine ()!);
-
-int count = 0;
-Console.WriteLine($" {count}");
-
-while (count < number)
-
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    
-count += 1;
-Console.WriteLine($"number {count}");
-//
-Console.WriteLine($"cube of number {count}" + Math.Pow(count, 3)); 
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+return result;
+}
+int Result(int[] a)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if(a[i] % 2 == 0)
+        {
+            count++;
+        }
 
- }
+    }
+    return count;
+}
+int[] array  = GetArray(13, 100, 999);
+Console.WriteLine(String.Join(", ", array));
+int count = Result(array);
+Console.WriteLine($"Quantity of even number:{count}");
