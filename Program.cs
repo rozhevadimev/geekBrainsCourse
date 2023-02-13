1d9aabc2a962c8ca0ddcff2661bcd8553da320e2
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-13 20:43:03 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,19 +1,26 @@
-// Задача 38: Задайте массив вещественных чисел.
-// Найдите разницу между максимальным и минимальным элементов массива
-Console.WriteLine("Please enter the size of the array:");
-int n = Convert.ToInt32(Console.ReadLine());
-int [] mass  = new int[n];
-Random rnd = new Random();
-for (int i = 0; i < n; i++)
-    { mass[i] = rnd.Next(-50, 50);
-    Console.Write($" {mass[i]} ");
-}
-int min = 0;
-int max = 0;
-foreach (int i in mass)  // Ищу максимальное и минимальное значения
+// Задание со *. Задать массив случайных чисел и 
+//отсортировать его методом пузырька по убыванию.
+int[] GetArray(int size, int minValue, int maxValue)
 {
-    if (min > i) min = i;
-    if (max < i) max = i;
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+return result;
 }
-Console.WriteLine($"\n Maximum array element: {max}\n Minimum array element: {min}");
-Console.WriteLine($"Difference between maximum and minimum element: {max-min}");
+
+int[]array = GetArray(12, -110,110);
+
+Console.WriteLine(String.Join(", ", array));
+for (int i = 0; i < array.Length - 1; i++)
+ for (int j = 0; j < array.Length - i - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                int temp = array[j];
+               array[j] = array[j + 1];
+               array[j + 1] = temp;
+            }
+        }
+        Console.WriteLine(String.Join(", ", array));
