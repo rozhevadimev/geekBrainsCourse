4a9e16317f12d364f04bbfc4d9492fb341c1b27f
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-15 23:27:17 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-// Задача 41: Пользователь вводит с клавиатуры M чисел. 
-//Посчитайте, сколько чисел больше 0 ввёл пользователь.
-Console.WriteLine("Please enter the number of numbers: ");
-int n = int.Parse(Console.ReadLine());
-int count = 0;
-for (int i = 0; i < n; i++)
-{
-    Console.WriteLine($"Please enter a number {i + 1}: ");
-    int x = int.Parse(Console.ReadLine());
-    if (x > 0)
-        count++;
-}
-Console.WriteLine(value: $"The user entered numbers greater than zero in quntity: {count}");
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine( "Please enter the required data to calculate the point of intersection of two lines");
+Console.WriteLine("Enter k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+ 
+if ((k1 * b2 - k2 * b1) == 0) 
+    Console.WriteLine($"Straight lines are parallel)");
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+Console.WriteLine($"The intersection is at: ({x};{y})");
