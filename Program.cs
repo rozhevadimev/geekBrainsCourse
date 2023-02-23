254744efc90cf4e3a4867e65028dbd98d8c90121
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-23 12:22:14 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,35 +1,46 @@
 // Задача 50. Напишите программу, которая на вход принимает 
 //позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, 
-//что такого элемента нет.(Первая часть - задача 47).
-Console.WriteLine("Enter how many rows the 2D array will have:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter how many columns the 2D array will have:");
+//что такого элемента нет.
+Console.WriteLine("Enter the coordinate of the array row");
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] twoDimArray = new double[m, n];
-Random rnd = new Random();
-void PrintArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { Console.Write($"{matr[i, j]} ");}
- Console.WriteLine();}}
+Console.WriteLine("Enter the coordinate of the array column");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [6,10];
+FillArrayRandomNumbers(numbers);
 
-void FillArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { matr[i,j] = Convert.ToDouble(rnd.Next(-2, 10)/20.0);}}}
-FillArray(twoDimArray);
-Console.WriteLine();
-Console.WriteLine("New 2D array: ");
-PrintArray(twoDimArray);
-Console.WriteLine();
-Console.WriteLine("Enter the coordinates of the element of the array");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m && b>n)
- Console.WriteLine("There is no number with such coordinates!");
- else
- {
- object c = twoDimArray.GetValue(a,b);
- Console.WriteLine(c);
- }
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("There is no number with such coordinates!");
+}
+else
+{
+    Console.WriteLine($"The value {n} of the row and column {m} of the array is  {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
