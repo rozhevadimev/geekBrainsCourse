3cf7101f35360666a7266b4ec3d89a0a57feecad
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-24 14:14:12 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,41 @@
-//Задача 52. Задайте двумерный массив из целых чисел. 
-//Найдите среднее арифметическое элементов в каждом столбце.
- Console.Clear();
-Random random = new Random();
-int[,] twoDimArray = new int[random.Next(1, 10), random.Next(1, 10)];
-for (int i = 0; i < twoDimArray.GetLength(0); i++)
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Console.Clear();
+Console.WriteLine("Enter quntity of rows: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter quntity of colums: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[n, m];
+FillArrayRandomNumbers(numbers);
+for (int j = 0; j < numbers.GetLength(1); j++)
 {
-    for (int j = 0; j < twoDimArray.GetLength(1); j++)
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
     {
-       twoDimArray[i, j] = random.Next(1, 10);
-        Console.Write(twoDimArray[i, j] + " ");
+        avarage = (avarage + numbers[i, j]);
     }
-    Console.WriteLine();
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
 }
-Console.WriteLine("---------------------------");
-Console.WriteLine(twoDimArray.GetLength(0));
-for (int j = 0; j < twoDimArray.GetLength(1); j++)
+Console.WriteLine();
+PrintArray(numbers);
+void FillArrayRandomNumbers(int[,] twoDimArray)
 {
-    double sum = 0;
     for (int i = 0; i < twoDimArray.GetLength(0); i++)
     {
-        sum += twoDimArray[i, j];
+        for (int j = 0; j < twoDimArray.GetLength(1); j++)
+        {
+            twoDimArray[i, j] = new Random().Next(0, 10);
+        }
     }
-    Console.Write($"{ sum / twoDimArray.GetLength(0)} ");
 }
-Console.ReadLine();
+void PrintArray(int[,] twoDimArray)
+{
+    for (int i = 0; i < twoDimArray.GetLength(0); i++)
+    {
+               for (int j = 0; j < twoDimArray.GetLength(1); j++)
+        {
+            Console.Write(twoDimArray[i, j] + " ");
+        }
+                Console.WriteLine("");
+    }
+}
