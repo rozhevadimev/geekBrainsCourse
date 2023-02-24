f9b6c3b22cf93b23c9f5296a97ef1d4435866305
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-24 13:58:08 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,46 +1,26 @@
-// Задача 50. Напишите программу, которая на вход принимает 
-//позиции элемента в двумерном массиве, 
-//и возвращает значение этого элемента или же указание, 8
-//что такого элемента нет.
-Console.WriteLine("Enter the coordinate of the array row");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the coordinate of the array column");
-int m = Convert.ToInt32(Console.ReadLine());
-int [,] numbers = new int [6,10];
-FillArrayRandomNumbers(numbers);
-
-if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+/ Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+ Console.Clear();
+Random random = new Random();
+int[,] twoDimArray = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < twoDimArray.GetLength(0); i++)
 {
-    Console.WriteLine("There is no number with such coordinates!");
+    for (int j = 0; j < twoDimArray.GetLength(1); j++)
+    {
+       twoDimArray[i, j] = random.Next(1, 10);
+        Console.Write(twoDimArray[i, j] + " ");
+    }
+    Console.WriteLine();
 }
-else
+Console.WriteLine("---------------------------");
+Console.WriteLine(twoDimArray.GetLength(0));
+for (int j = 0; j < twoDimArray.GetLength(1); j++)
 {
-    Console.WriteLine($"The value {n} of the row and column {m} of the array is  {numbers[n-1,m-1]}");
-}
-
-PrintArray(numbers);
-
-void FillArrayRandomNumbers(int[,] twoDimArray)
-{
-    for (int i = 0; i < twoDimArray.GetLength(0); i++)
-        {        
-            for (int j = 0; j < twoDimArray.GetLength(1); j++)
-            {
-                twoDimArray [i,j] = new Random().Next(-100, 100)/10;
-            }   
-        }
-}
-
-void PrintArray(int[,] twoDimArray)
-{
+    double sum = 0;
     for (int i = 0; i < twoDimArray.GetLength(0); i++)
     {
-        Console.Write("[ ");
-        for (int j = 0; j < twoDimArray.GetLength(1); j++)
-        {
-            Console.Write(twoDimArray[i,j] + " ");
-        }   
-        Console.Write("]");
-        Console.WriteLine(""); 
+        sum += twoDimArray[i, j];
     }
+    Console.Write($"{ sum / twoDimArray.GetLength(0)} ");
 }
+Console.ReadLine();
