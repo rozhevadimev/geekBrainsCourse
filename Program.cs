c7fe27dcdb8e858cc752c2e67a57f7f86ec25553
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-24 18:19:53 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,61 +1,85 @@
-// Задача 58: Задайте две матрицы. Напишите программу, 
-//которая будет находить произведение двух матриц.;
-Console.Clear();
-int m = InputNumbers("Enter the number of rows of the 1st matrix: ");
-int n = InputNumbers("Enter the number of columns of the 1st matrix (and rows of the 2nd matrix): ");
-int p = InputNumbers("Enter the number of columns of the 2nd matrix: ");
-int range = InputNumbers("Enter a range of random numbers: from 1 to ");
-int[,] firstMartrix = new int[m, n];
-CreateArray(firstMartrix);
-Console.WriteLine($"First matrix: ");
-WriteArray(firstMartrix);
-int[,] secomdMartrix = new int[n, p];
-CreateArray(secomdMartrix);
-Console.WriteLine($"Second matrix: ");
-WriteArray(secomdMartrix);
-int[,] resultMatrix = new int[m,p];
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-Console.WriteLine($"Multiplication of the first and second matrices: ");
-WriteArray(resultMatrix);
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+ static class MatrixExt
 {
-  for (int i = 0; i < resultMatrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        public static int RowsCount(this int[,] matrix)
     {
-      int sum = 0;
-      for (int k = 0; k < firstMartrix.GetLength(1); k++)
-      {
-        sum += firstMartrix[i,k] * secomdMartrix[k,j];
-      }
-      resultMatrix[i,j] = sum;
+        return matrix.GetUpperBound(0) + 1;
     }
-  }
-}
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+ public static int ColumnsCount(this int[,] matrix)
     {
-      array[i, j] = new Random().Next(range);
-    }
-  }
-}
-void WriteArray (int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+        return matrix.GetUpperBound(1) + 1;
+    }   
+        static int[,] GetMatrixFromConsole(string name)
     {
-      Console.Write(array[i,j] + " ");
+        Console.Write("Quantity of rows of matrix {0}:    ", name);
+        var n = int.Parse(Console.ReadLine());
+        Console.Write("Quantity of colums of matrix {0}: ", name);
+        var m = int.Parse(Console.ReadLine());
+ 
+        var matrix = new int[n, m];
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                Console.Write("{0}[{1},{2}] = ", name, i, j);
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+ 
+        return matrix;
     }
-    Console.WriteLine();
-  }
+     static void PrintMatrix(int[,] matrix)
+    {
+        for (var i = 0; i < matrix.RowsCount(); i++)
+        {
+            for (var j = 0; j < matrix.ColumnsCount(); j++)
+            {
+                Console.Write(matrix[i, j].ToString().PadLeft(4));
+            }
+ 
+            Console.WriteLine();
+        }
+    }
+     static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {       
+        if (matrixA.ColumnsCount() != matrixB.RowsCount())
+        {
+            Console.WriteLine("Multiplication is impossible! The number of columns in the first matrix is not equal to the number of rows in the second matrix.");
+                }
+        var matrixC = new int[matrixA.RowsCount(), matrixB.ColumnsCount()];
+         for (var i = 0; i < matrixA.RowsCount(); i++)
+        {
+            for (var j = 0; j < matrixB.ColumnsCount(); j++)
+            {
+                matrixC[i, j] = 0;
+                 for (var k = 0; k < matrixA.ColumnsCount(); k++)
+                {
+                    matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+         return matrixC;
+    }
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Program of Matrix Multiplication");
+ 
+        var a = GetMatrixFromConsole("A");
+        var b = GetMatrixFromConsole("B");
+ 
+        Console.WriteLine("Matrix A:");
+        PrintMatrix(a);
+ 
+        Console.WriteLine("Matrix B:");
+        PrintMatrix(b);
+ if (a.ColumnsCount() != b.RowsCount())
+
+  Console.WriteLine("Multiplication is impossible!");
+else
+        MatrixMultiplication(a, b);
+        Console.WriteLine("Matrix multiplication:");
+        PrintMatrix(MatrixMultiplication(a, b));
+ 
+        Console.ReadLine();
+    }
 }
