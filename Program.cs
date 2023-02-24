989d988e7f69c693a18c2c531dab9c3e3fd1764a
Винников Вадим Эдуардович <122885964+rozhevadimev@users.noreply.github.com>
2023-02-24 15:21:08 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,58 +1,35 @@
-//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
-//по убыванию элементы каждой строки двумерного массива.
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
 Console.Clear();
-int m = InputNumbers("Enter quantity of colums m: ");
-int n = InputNumbers("Ente quntity of rows n: ");
-int range = InputNumbers("Enter a range of numbers: from 1 to ");
-int[,] twoDimArray = new int[m, n];
-CreateArray(twoDimArray);
-WriteArray(twoDimArray);
-Console.WriteLine($"\n Descending sorted array: ");
-OrderArrayLines(twoDimArray);
-WriteArray(twoDimArray);
-void OrderArrayLines(int[,] twoDimArray)
-{
-  for (int i = 0; i < twoDimArray.GetLength(0); i++)
-  {
-    for (int j = 0; j < twoDimArray.GetLength(1); j++)
-    {
-      for (int k = 0; k < twoDimArray.GetLength(1) - 1; k++)
-      {
-        if (twoDimArray[i, k] < twoDimArray[i, k + 1])
-        {
-          int temp = twoDimArray[i, k + 1];
-          twoDimArray[i, k + 1] = twoDimArray[i, k];
-          twoDimArray[i, k] = temp;
-        }
-      }
-    }
-  }
-}
-int InputNumbers(string input)
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}
-
-void CreateArray(int[,] twoDimArray)
-{
-  for (int i = 0; i < twoDimArray.GetLength(0); i++)
-  {
-    for (int j = 0; j < twoDimArray.GetLength(1); j++)
-    {
-      twoDimArray[i, j] = new Random().Next(range);
-    }
-  }
-}
-void WriteArray(int[,] twoDimArray)
-{
-  for (int i = 0; i < twoDimArray.GetLength(0); i++)
-  {
-    for (int j = 0; j < twoDimArray.GetLength(1); j++)
-    {
-      Console.Write(twoDimArray[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-}
+int n1, n2;
+            int[,] A;
+             Console.Write("Enter the number of rows in the matrix: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number of colums in the matrix: ");
+            n2 = int.Parse(Console.ReadLine());
+            A = new int[n1, n2];
+            Random rnd = new Random();
+ 
+            for (int i = 0; i < n1; i++)
+                for (int j = 0; j < n2; j++)
+                    A[i, j] = rnd.Next(-0, 10 + 1);
+ 
+                        for (int i = 0; i < n1; i++, Console.WriteLine())
+                for (int j = 0; j < n2; j++)
+                    Console.Write(A[i, j] + "\t");
+int minRowSum = int.MaxValue, indexMinRow = 0;
+ 
+             for (int i = 0; i < n1; i++)
+             {
+                int rowSum = 0;
+                for (int j = 0; j < n2; j++)
+                    rowSum += A[i, j];
+                                if (rowSum < minRowSum)
+                {
+                    minRowSum = rowSum;
+                    indexMinRow = i;
+                }
+             }
+              Console.WriteLine("Row with minimum sum of matrix elements");
+                 for(int j = 0;j<n2;j++)
+                      Console.Write(A[indexMinRow, j] + "\t");
